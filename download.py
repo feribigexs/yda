9fc259a8106df66bd95b5c38ebc50129a5ffd6ec
feribigexs <92066249+feribigexs@users.noreply.github.com>
2023-12-12 19:53:02 +0700

    Update download.py

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 import requests
 import os
 os.system("rm yda-master -Rf")
-url = "https://gitlab.zam.io/wajinhakim/yda/-/archive/master/yda-master.tar.gz"
+url = "https://github.com/feribigexs/yda/-/archive/main/yda-master.tar.gz"
 r = requests.get(url, allow_redirects=True, verify=False)
 open('yda-master.tar.gz', 'wb').write(r.content)
 os.system("tar xvf yda-master.tar.gz && rm yda-master.tar.gz")
